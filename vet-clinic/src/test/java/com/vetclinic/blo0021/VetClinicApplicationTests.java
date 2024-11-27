e286ebe7c90d372d4a17dcc6dae5613055670706
Kseniia Blokhina <ksublokhina@gmail.com>
2024-11-27 19:53:32 +0100

    test

---
+++ vet-clinic/src/test/java/com/vetclinic/blo0021/VetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetclinic.blo0021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
